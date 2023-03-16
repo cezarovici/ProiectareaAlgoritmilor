7a0ca9d4d0f0e326180287a119a5bdca63e84c14
Apetroaei Cezar <njroffic@gmail.com>
2023-03-16 13:35:26 +0200

    Solved lab4

--- Lab4/DigitalTries.cpp
+++ Lab4/DigitalTries.cpp
@@ -3,58 +3,80 @@
 
 using namespace std;
 
+#define N 26
+
 struct Nod
 {
     bool endw = false;
     Nod *next[26] = {nullptr};
 };
 
-void init(Nod *&root)
-{
-    root = new Nod;
-    root->endw = false;
+
+Nod* createNode(){
+    Nod* newNode = new Nod;
+
+    for (int i = 0 ; i < N ; i++){
+        newNode->next[i] = nullptr;
+    }
+
+    newNode->endw = false;
+
+    return newNode;
 }
 
 void Insert(Nod *root, const char *word)
 {
+    Nod* temp = root;
+
+    for(int i = 0 ; word[i]!='\0';i++){
+        int index = word[i] - 'a';
+
+        if (temp->next[index] == nullptr){
+            temp->next[index] = createNode();
+        }
+
+        temp = temp->next[index];
+    }
+
+    temp->endw = true;
+}
+
+bool Search(Nod *root, const char *word)
+{
+
+    cout<<"\n Searching : "<< word;
+
     if (root == nullptr)
     {
-        init(root);
+        cout << "\n Empty tree";
+
+        return false;
     }
 
     Nod *temp = root;
 
     for (int i = 0; i < strlen(word); i++)
     {
-        while (temp->next[word[i] - 'a'] != nullptr)
-        {
-            temp = temp->next[word[i] - 'a'];
-        }
-
         if (temp->next[word[i] - 'a'] == nullptr)
         {
-            temp->next[word[i] - 'a'] = new Nod;
+            return false;
         }
+
+        temp = temp->next[word[i] - 'a'];
     }
 
-    temp->endw = true;
-}
-
-void Search(Nod *root, int m, const char *word)
-{
-    int i = 0;
-    Nod *temp = root;
-
-    while (temp != nullptr && i < m)
-    {
-    }
+    return (temp != nullptr && temp->endw);
 }
 
 int main()
 {
-    Nod *tree = nullptr;
+    Nod *tree = new Nod;
+
     Insert(tree, "cezar");
     Insert(tree, "cez");
+    Insert(tree,"mihai");
 
-    
+    cout << Search(tree, "cezar");
+    cout << Search(tree,"mih");
+    cout<< Search(tree,"mihai");
 }

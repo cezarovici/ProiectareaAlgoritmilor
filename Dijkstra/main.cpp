c0157d36c5bc298533ba4b73d9b7d384ade39414
Apetroaei Cezar <njroffic@gmail.com>
2023-05-09 21:47:42 +0300

    init Dijkstra

---
+++ Dijkstra/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+const int infinit = 9999999;
+int matrix[6][6] = {
+    {0,1,2,5,infinit,infinit},
+    {infinit,0,1,infinit,3,infinit},
+    {infinit,infinit,0,1,1,infinit},
+    {infinit,infinit,infinit,0,infinit,infinit},
+    {}
+    };
+
+int main(){
+
+    return 0; 
+}

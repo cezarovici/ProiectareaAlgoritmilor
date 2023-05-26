4db7bf34c0f97cc9e2e8e26759f69802a7377b99
Apetroaei Cezar <njroffic@gmail.com>
2023-05-27 00:20:41 +0300

    bag problem solved

--- L13_ProblemaRucsacului/main.cpp
+++ L13_ProblemaRucsacului/main.cpp
@@ -33,10 +33,77 @@ void interclass(vector <store>s,vector<store> t,int max, vector<store> &r){
             j++;
         }
     }
+
+    while (i < s.size()) {
+        if (s[i].w <= max && (r.size() == 0 || s[i].p > r.back().p)) {
+            r.push_back(s[i]);
+        }
+        i++;
+    }
+
+    while (j < t.size()) {
+        if (t[j].w <= max && (r.size() == 0 || t[j].p > r.back().p)) {
+            r.push_back(t[j]);
+        }
+        j++;
+    }
 }
 
-int main(){
+void rucsac(int M, int w[], int p[], int x[]) {
+    vector <store> S[m];
+    vector <store> T[m];
 
+    store temp;
+
+    temp.w = 0;
+    temp.p = 0;
+    
+    S[0].push_back(temp);
+
+    temp.w = w[1];
+    temp.p = p[1];
+
+    T[0].push_back(temp);
+
+    for (int i = 1; i <= n; i++) {
+        vector <store> temp;
+        interclass(S[i - 1], T[i - 1], M, S[i]);
+
+        for (int j = 0; j < S[i].size(); j++) {
+            store temp;
+            temp.w = S[i].at(j).w + w[i];
+            temp.p = S[i].at(j).p + p[i];
+            T[i].push_back(temp);
+        }
+
+    }
+    
+    store xy = S[n].back();
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (xy.w == S[i].back().w && xy.p == S[i].back().p) {
+            x[i + 1] = 0;
+        }
+        else {
+            x[i + 1] = 1;
+            xy.w = xy.w - w[i + 1];
+            xy.p = xy.p - p[i + 1];
+        }
+    }
+}
+
+
+int main(){
+    vector <store> S[4];
+   
+    int w[3] = { 3, 5, 6 };
+    int p[3] = { 10, 30, 20 };
+    int x[5];
+
+    rucsac(m, w, p, x);
+    for (int i = 0; i < n; i++) {
+        cout << x[i] << " ";
+    }
 
 
     return 0;
